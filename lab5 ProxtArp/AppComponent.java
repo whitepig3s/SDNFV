79dc77dfe62a8040f3d1bc91fb15dc6dc855ce86
whitepig3s <whitepig3s@gmail.com>
2020-11-30 17:34:55 +0800

    少一次reply

--- lab5 ProxtArp/AppComponent.java
+++ lab5 ProxtArp/AppComponent.java
@@ -11,20 +11,26 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.onosproject.net.packet.DefaultOutboundPacket;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.TrafficTreatment;
 import org.onosproject.net.packet.PacketProcessor;
 import org.onosproject.cfg.ComponentConfigService;
 import org.onosproject.net.packet.PacketService;
 import org.onosproject.net.packet.PacketContext;
 import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.PortNumber;
 import org.onosproject.net.HostId;
 import org.onlab.packet.Ethernet;
 import org.onlab.packet.ARP;
 import org.onlab.packet.MacAddress;
 import org.onlab.packet.IPv4;
+import org.onlab.packet.Ip4Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.Dictionary;
+import java.nio.ByteBuffer;
 
 import static org.onlab.util.Tools.get;
 
@@ -36,7 +42,7 @@ public class AppComponent {
 
     private PacketProcessor processor;
 
-    protected Map<IPv4, MacAddress> arpTable = Maps.newConcurrentMap();
+    protected Map<Ip4Address, MacAddress> arpTable = Maps.newConcurrentMap();
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected CoreService coreService;
@@ -59,26 +65,47 @@ public class AppComponent {
     }
 
     private class ProxyProcessor implements PacketProcessor {
+        Ethernet req;
+        ConnectPoint srcConnectPoint ;
+
         @Override
         public void process(PacketContext pc) {
             Ethernet eth = pc.inPacket().parsed();
-            //ConnectPoint cp = pc.inPacket().receivedFrom();
-            //HostId hid = cp.hostId();
-            ARP arpPacket = (ARP) eth.getPayload();
-
             if (eth.getEtherType() == eth.TYPE_ARP) {
+                ARP arpPacket = (ARP) eth.getPayload();
+                Ip4Address srcip = Ip4Address.valueOf(arpPacket.getSenderProtocolAddress());
+                Ip4Address dstip = Ip4Address.valueOf(arpPacket.getTargetProtocolAddress());
+                MacAddress srcmac = eth.getSourceMAC();
+                if (arpPacket.getOpCode() == ARP.OP_REQUEST) {
+                    req = eth;
+                    srcConnectPoint=pc.inPacket().receivedFrom();
+                    log.info("request");
+                    if (!arpTable.containsKey(dstip)) {
+                        log.info("TABLE MISS. Send request to edge ports");
+                        pc.treatmentBuilder().setOutput(PortNumber.FLOOD);
+                        pc.send();
+                        process(pc);
+                    } else {
+                        log.info("YES");
+                        Ethernet ethReply = ARP.buildArpReply(dstip, arpTable.get(dstip), req);
+                        TrafficTreatment.Builder builder = DefaultTrafficTreatment.builder();
+                        builder.setOutput(srcConnectPoint.port());
+                        packetService.emit(new DefaultOutboundPacket(srcConnectPoint.deviceId(), builder.build(),
+                                ByteBuffer.wrap(ethReply.serialize())));
 
-                // final ARP a=(ARP)eth.deserialize().getPayload();
-                 log.info("ETH: {}",arpPacket.getSenderProtocolAddress());
-                //log.info("ETH: {}",hid.mac());
+                    }
+                } else if (arpPacket.getOpCode() == ARP.OP_REPLY) {
+                    addARP(srcip, srcmac);
+                    log.info("get REPLY ");
+                }
 
-            } else if (eth.getEtherType() == eth.TYPE_IPV4) {
-                // IPv4 ipPacket = (IPv4) eth.getPayload();
             }
+
+            // log.info("ETH: {}",IPv4.toIPv4Address(arpPacket.getSenderProtocolAddress()));
 
         }
 
-        private void addARP(IPv4 ip, MacAddress srcMac) {
+        private void addARP(Ip4Address ip, MacAddress srcMac) {
             arpTable.putIfAbsent(ip, srcMac);
             log.info("Add IP address: {},MAC: {}", ip, srcMac);
 
